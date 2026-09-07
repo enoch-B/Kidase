abf1644c1564a2c75ac0dd6f4a2fda28d98d3b10
enoch <henokt828@gmail.com>
2026-09-07 11:27:32 +0300

    chore: project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kidase"
+    namespace = "com.enoch.kidase"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.kidase"
+        applicationId = "com.enoch.kidase"
         minSdk = 25
         targetSdk = 36
         versionCode = 1

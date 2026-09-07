6c393bc8acaffbb8626fb8a4c2bf18c89ce9f444
enoch <henokt828@gmail.com>
2026-09-07 13:28:39 +0300

    feat: implement AudioPlayerViewModel with Media3 ExoPlayer for audio playback and progress tracking

--- app/src/main/java/com/enoch/kidase/ui/theme/Color.kt
+++ app/src/main/java/com/enoch/kidase/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.enoch.kidase.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Maroon = Color(0xFF5C1A22)
+val DeepMaroon = Color(0xFF3E1015)
+val GoldOchre = Color(0xFFE8B84B)
+val WarmGoldBrown = Color(0xFFC9862E)
+val Cream = Color(0xFFFBF6EC)
+val CardBackground = Color(0xFFFFFFFF)
+val CardBorder = Color(0xFFE3D3AC)
